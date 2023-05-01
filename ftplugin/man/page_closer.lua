142b9e44336a2e46b14db51e362b5c56217f7860
Lampros <hauahx@gmail.com>
2023-05-02 02:01:58 +0300

    added toc, and a few snippets

---
+++ ftplugin/man/page_closer.lua
@@ -0,0 +1,17 @@
+--[[
+-- Contributor: LamprosPitsillos
+-- Date : 2023-05-02
+-- Description : Usefull for those using neovim for man page viewer, makes exiting more intuitive
+--]]
+
+vim.keymap.set("n", "q", function()
+    local bufs_all = vim.api.nvim_list_bufs()
+    local bufs_listed = vim.tbl_filter(function(buf)
+        if (vim.fn.buflisted(buf) == 1) then return true
+        else return false
+        end
+    end, bufs_all)
+    if (#bufs_listed == 1) then vim.api.nvim_cmd({ cmd = "quit" }, {})
+    else vim.api.nvim_buf_delete(0, {})
+    end
+end)
--- lua/open_file_with_xdg_open.lua
+++ lua/open_file_with_xdg_open.lua
@@ -1,3 +1,9 @@
+--[[
+-- Contributor: LamprosPitsillos
+-- Date : 2023-05-02
+-- Description : A small autocommand to open pdfs using neovim from your xdg assigned viewer
+--]]
+
 vim.api.nvim_create_autocmd({ "BufReadPost" }, {
     pattern = "*.pdf",
     callback = function(ev)
@@ -5,7 +11,6 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
         vim.fn.jobstart({ "xdg-open", filename }, { detach = true })
         -- require("mini.bufremove").delete(0, false) 
         -- Using mini.remove results in the layout staying consistent
-        --
         vim.api.nvim_buf_delete(0,{})
 
     end
